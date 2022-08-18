4438b2c4407e6d1810c81a811c2c4843fb8430b6
unknown <cogjava3109@iiht.tech>
2022-08-18 10:43:53 +0530

    added service

---
+++ src/main/java/com/emp/service/EmployeeServiceImpl.java
@@ -0,0 +1,36 @@
+package com.emp.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.emp.entity.Employee;
+
+@Service
+public class EmployeeServiceImpl implements IEmployeeService {
+	
+	//Dummy Employee List
+	
+	List<Employee> list = List.of(
+			new Employee(1311L,"sachin","8937283822"),
+			new Employee(1312L,"akshay","8937283824"),
+			new Employee(1313L,"ashish","8937923822"),
+			new Employee(1314L,"dhanesh","8936283822"),
+			new Employee(1315L,"shubham","8924283822"),
+			new Employee(1316L,"rajat","8937283822"),
+			new Employee(1317L,"krishna","8937283822"),
+			new Employee(1318L,"soham","8937283822"),
+			new Employee(1319L,"sumit","8937283822")
+			);
+	
+
+	@Override
+	public Employee getEmployee(Long id) {
+		return list.stream().filter(employee->employee.geteId().equals(id)).findAny().orElse(null);
+	}
+	
+	
+	
+	
+
+}
