da74c7f4fa6c5e4c10f79d9f9eb5f9e019a6d3e6
unknown <cogjava3109@iiht.tech>
2022-08-18 10:22:10 +0530

    first commit

---
+++ src/main/java/com/emp/entity/Employee.java
@@ -0,0 +1,67 @@
+package com.emp.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employee {
+	
+	private Long eId;
+	private String name;
+	private String phone;
+	
+	List<Record> records = new ArrayList<>();
+
+	public Long geteId() {
+		return eId;
+	}
+
+	public void seteId(Long eId) {
+		this.eId = eId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public List<Record> getRecord() {
+		return records;
+	}
+
+	public void setRecord(List<Record> record) {
+		this.records = record;
+	}
+
+	public Employee(Long eId, String name, String phone, List<Record> record) {
+		super();
+		this.eId = eId;
+		this.name = name;
+		this.phone = phone;
+		this.records = record;
+	}
+
+	public Employee(Long eId, String name, String phone) {
+		super();
+		this.eId = eId;
+		this.name = name;
+		this.phone = phone;
+	}
+
+	public Employee() {
+		super();
+	}
+	
+	
+
+}
